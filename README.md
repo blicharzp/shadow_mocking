03bc8e21972e9eab9318911ce0cb268077769f55
Pawel Blicharz <pblicharz@luxoft.com>
2019-08-07 14:01:37 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# shadow_mocking
+
+## Default build type:
+
+`cmake ..`
+
+## Test build type:
+
+`cmake .. -DDUPA=TEST`
---
+++ foo_test.cpp
@@ -0,0 +1,7 @@
+#include <foo.hpp>
+#include <iostream>
+
+void foo_function()
+{
+    std::cout << "FOO_TEST" << std::endl;
+}
